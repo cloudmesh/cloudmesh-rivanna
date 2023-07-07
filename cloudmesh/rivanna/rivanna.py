b112c4f55a98df3229410ce2b3c7f247ed31b319
Gregor von Laszewski <laszewski@gmail.com>
2023-07-06 21:42:50 -0400

    fix size report

--- cloudmesh/rivanna/rivanna.py
+++ cloudmesh/rivanna/rivanna.py
@@ -186,7 +186,7 @@ class Rivanna:
             Shell.rm ("output_image.sif")
             Shell.rm ("build.def")
             StopWatch.stop("build image")
-            size = Shell.run(f"du -sh {image}")
+            size = Shell.run(f"du -sh {image}").split()[0]
 
             timer = StopWatch.get("build image")
             print()
